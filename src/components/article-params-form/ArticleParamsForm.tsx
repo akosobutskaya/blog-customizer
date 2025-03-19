b16f096d0709d2aa2ad7c2c3a488f463ce2ce08b
Hanna Kasabutskaya <akosobutskaya@yandex-team.ru>
2025-03-19 22:52:07 +0300

    v1

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,16 +1,113 @@
 import { ArrowButton } from 'src/ui/arrow-button';
 import { Button } from 'src/ui/button';
+import { FormEvent, useRef, useState, useCallback } from 'react';
+import { Text } from 'src/ui/text/Text';
+import { Select } from 'src/ui/select/Select';
+import { RadioGroup } from 'src/ui/radio-group/RadioGroup';
+import { Separator } from 'src/ui/separator/Separator';
+import {
+	OptionType,
+	fontFamilyOptions,
+	ArticleStateType,
+	fontSizeOptions,
+	fontColors,
+	backgroundColors,
+	contentWidthArr,
+} from 'src/constants/articleProps';
+import { useClose } from './hook/useClose';
+import clsx from 'clsx';
 
 import styles from './ArticleParamsForm.module.scss';
 
-export const ArticleParamsForm = () => {
+type ArticleParamsFormProps = {
+	fontFamily: (select: OptionType) => void;
+	fontSize: (select: OptionType) => void;
+	fontColor: (select: OptionType) => void;
+	backgroundColor: (select: OptionType) => void;
+	contentWidth: (select: OptionType) => void;
+	resetButton: () => void;
+	applyButton: (event: FormEvent) => void;
+	sideBarState: ArticleStateType;
+};
+
+export const ArticleParamsForm = ({
+	fontFamily,
+	fontSize,
+	fontColor,
+	backgroundColor,
+	contentWidth,
+	resetButton,
+	applyButton,
+	sideBarState,
+}: ArticleParamsFormProps) => {
+	const ref = useRef<HTMLFormElement | null>(null);
+	const [open, setOpen] = useState(false);
+
+	useClose({
+		isOpen: open,
+		onClose: () => setOpen(false),
+		rootRef: ref,
+	});
+
+	const toggleForm = useCallback(() => {
+		setOpen((prevOpen) => !prevOpen);
+	}, []);
+
 	return (
 		<>
-			<ArrowButton isOpen={false} onClick={() => {}} />
-			<aside className={styles.container}>
-				<form className={styles.form}>
-					<div className={styles.bottomContainer}>
-						<Button title='Сбросить' htmlType='reset' type='clear' />
+			<ArrowButton isOpen={open} onClick={toggleForm} />
+			<aside
+				className={clsx(styles.container, { [styles.container_open]: open })}>
+				<form className={styles.form} ref={ref} onSubmit={applyButton}>
+					<Text size={31} weight={800} uppercase as={'h3'} align='center'>
+						Задайте параметры
+					</Text>
+					<span className={styles.span} />
+					<Select
+						selected={sideBarState.fontFamilyOption}
+						options={fontFamilyOptions}
+						onChange={fontFamily}
+						title='Шрифт'
+					/>
+					<span className={styles.span} />
+					<RadioGroup
+						name='fontSize'
+						options={fontSizeOptions}
+						selected={sideBarState.fontSizeOption}
+						onChange={fontSize}
+						title='Размер'
+					/>
+					<span className={styles.span} />
+					<Select
+						selected={sideBarState.fontColor}
+						options={fontColors}
+						onChange={fontColor}
+						title='Цвет шрифта'
+					/>
+					<span className={styles.span} />
+					<Separator />
+					<span className={styles.span} />
+					<Select
+						selected={sideBarState.backgroundColor}
+						options={backgroundColors}
+						onChange={backgroundColor}
+						title='Цвет фона'
+					/>
+					<span className={styles.span} />
+					<Select
+						selected={sideBarState.contentWidth}
+						options={contentWidthArr}
+						onChange={contentWidth}
+						title='Ширина контента'
+					/>
+					<span className={styles.span} />
+					<div className={clsx(styles.bottomContainer)}>
+						<Button
+							title='Сбросить'
+							htmlType='reset'
+							type='clear'
+							onClick={resetButton}
+						/>
 						<Button title='Применить' htmlType='submit' type='apply' />
 					</div>
 				</form>
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,14 @@
 import { createRoot } from 'react-dom/client';
-import { StrictMode, CSSProperties } from 'react';
+import { StrictMode, CSSProperties, FormEvent, useState } from 'react';
 import clsx from 'clsx';
 
 import { Article } from './components/article/Article';
 import { ArticleParamsForm } from './components/article-params-form/ArticleParamsForm';
-import { defaultArticleState } from './constants/articleProps';
+import {
+	ArticleStateType,
+	OptionType,
+	defaultArticleState,
+} from './constants/articleProps';
 
 import './styles/index.scss';
 import styles from './styles/index.module.scss';
@@ -13,19 +17,62 @@ const domNode = document.getElementById('root') as HTMLDivElement;
 const root = createRoot(domNode);
 
 const App = () => {
+	const [sideBarState, setSideBarState] =
+		useState<ArticleStateType>(defaultArticleState);
+	const [state, setState] = useState(defaultArticleState);
+
+	const changeFontFamily = (select: OptionType) => {
+		setSideBarState({ ...sideBarState, fontFamilyOption: select });
+	};
+
+	const changeFontSize = (select: OptionType) => {
+		setSideBarState({ ...sideBarState, fontSizeOption: select });
+	};
+
+	const changeFontColor = (select: OptionType) => {
+		setSideBarState({ ...sideBarState, fontColor: select });
+	};
+
+	const changeContainerWidth = (select: OptionType) => {
+		setSideBarState({ ...sideBarState, contentWidth: select });
+	};
+
+	const changeBgColor = (select: OptionType) => {
+		setSideBarState({ ...sideBarState, backgroundColor: select });
+	};
+
+	const resetSidebarState = () => {
+		setState(defaultArticleState);
+		setSideBarState(defaultArticleState);
+	};
+
+	const applySideBarState = (event: FormEvent) => {
+		event.preventDefault();
+		setState(sideBarState);
+	};
+
 	return (
 		<main
 			className={clsx(styles.main)}
 			style={
 				{
-					'--font-family': defaultArticleState.fontFamilyOption.value,
-					'--font-size': defaultArticleState.fontSizeOption.value,
-					'--font-color': defaultArticleState.fontColor.value,
-					'--container-width': defaultArticleState.contentWidth.value,
-					'--bg-color': defaultArticleState.backgroundColor.value,
+					'--font-family': state.fontFamilyOption.value,
+					'--font-size': state.fontSizeOption.value,
+					'--font-color': state.fontColor.value,
+					'--container-width': state.contentWidth.value,
+					'--bg-color': state.backgroundColor.value,
 				} as CSSProperties
 			}>
-			<ArticleParamsForm />
+			<ArticleParamsForm
+				fontFamily={changeFontFamily}
+				fontSize={changeFontSize}
+				fontColor={changeFontColor}
+				backgroundColor={changeBgColor}
+				contentWidth={changeContainerWidth}
+				resetButton={resetSidebarState}
+				applyButton={applySideBarState}
+				sideBarState={sideBarState}
+			/>
 			<Article />
 		</main>
 	);
